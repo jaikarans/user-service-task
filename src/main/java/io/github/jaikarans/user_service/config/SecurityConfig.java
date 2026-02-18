d5537f2b0f511fdc3b5e0c1a7f3a0a98115ef025
Jaikarans <jaikrns@gmail.com>
2026-02-18 12:07:19 +0530

    fix h2 db for deploy test

--- src/main/java/io/github/jaikarans/user_service/config/SecurityConfig.java
+++ src/main/java/io/github/jaikarans/user_service/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login", "/api/hi").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
